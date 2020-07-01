d3230672187218502e1cb134eec1934b6b68baf3
AkhtarZainab <zainabakhtar27@gmail.com>
2020-07-01 19:47:50 +0530

    Commit for UC-2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,7 +6,9 @@ randomCheck=$(( RANDOM%2 ))
 
 if [ $isPresent -eq $randomCheck ]
 then
-	echo "Employee is Present"
+	empRatePerHr=20
+	empHrs=8
+	salary=$(( $empRatePerHr*$empHrs ))
 else
-	echo "Employee is Absent"
+	salary=0
 fi
